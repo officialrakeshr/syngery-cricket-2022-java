89ea5f5b97c4fef93e6c43129ed60b264cb4b708
Your Name <you@example.com>
2023-03-28 10:08:00 +0530

    updated logging

--- src/main/java/com/aztechsynergy/crickScore/config/RequestLoggingFilterConfig.java
+++ src/main/java/com/aztechsynergy/crickScore/config/RequestLoggingFilterConfig.java
@@ -11,10 +11,10 @@ public class RequestLoggingFilterConfig {
     public CommonsRequestLoggingFilter logFilter() {
         CommonsRequestLoggingFilter filter
                 = new CommonsRequestLoggingFilter();
+        filter.setIncludeClientInfo(true);
         filter.setIncludeQueryString(true);
         filter.setIncludePayload(true);
-        filter.setMaxPayloadLength(10000);
-        filter.setIncludeHeaders(true);
+        filter.setMaxPayloadLength(20000);
         filter.setAfterMessagePrefix("REQUEST DATA: ");
         return filter;
     }
